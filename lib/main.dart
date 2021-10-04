b84013ac6aeb5bc10d63e473cbd8d8923d14c3f7
Sahil Ahir <sahilahir333@gmail.com>
2021-10-04 20:35:54 +0530

    complete Dicee with stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int leftbutton = 1;
+  int rightbutton = 1;
+
+  void changedice() {
+    setState(() {
+      leftbutton = Random().nextInt(6) + 1;
+      rightbutton = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +40,16 @@ class MyApp extends StatelessWidget {
               Expanded(
                 child: TextButton(
                     onPressed: () {
-                      print('Left Pressed');
+                      changedice();
                     },
-                    child: Image.asset('images/dice1.png')),
+                    child: Image.asset('images/dice$leftbutton.png')),
               ),
               Expanded(
                 child: TextButton(
                     onPressed: () {
-                      print('Right Pressed');
+                      changedice();
                     },
-                    child: Image.asset('images/dice1.png')),
+                    child: Image.asset('images/dice$rightbutton.png')),
               ),
             ],
           ),
